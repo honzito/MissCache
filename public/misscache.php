73032176ebfc18dc4e89feb9e61da28243b3f342
honzito <honzito@ecn.cz>
2025-09-10 17:28:05 +0200

    fixed alerts_sending.php parameter type mis-match

--- public/misscache.php
+++ public/misscache.php
@@ -8,7 +8,7 @@ $documentRoot = rtrim($_SERVER['DOCUMENT_ROOT'], '/');
 $requestUri   = $_GET['fpath'] ?? '';        // e.g. "/phpThumbCache/img_upload/xy/..."
 $cacheRootUri = '/';                          // not used directly; kept for clarity
 
-$mC = new MissCache\Util\Util\Util\MissChache( [new PhpThumbPlugin($documentRoot . '/aaa/img.php')] );
+$mC = new MissCache\MissChache( [new PhpThumbPlugin($documentRoot . '/aaa/img.php')] );
 // new SpatieImagePlugin()
 
 $router = new PluginRouter(
